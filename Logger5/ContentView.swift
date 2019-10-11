268af2831bd25de81e5ebc62bcc8888f3273eec5
Satoshi Kobayashi <pikanchisato@yahoo.co.jp>
2019-10-11 16:49:56 +0900

    make new project to SwiftUI

--- Logger5/ContentView.swift
+++ Logger5/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading) {
+            Text("Accelerometer")
+            Text("Gyroscope")
+            Text("Magnetometer")
+        }
     }
 }
 
