4b0dd46a1a00258e4f017c45bf4db5cd984b04d8
Satoshi Kobayashi <pikanchisato@yahoo.co.jp>
2019-10-11 17:35:14 +0900

    add some components

--- Logger5/ContentView.swift
+++ Logger5/ContentView.swift
@@ -9,11 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var logStarting = false
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Accelerometer")
-            Text("Gyroscope")
-            Text("Magnetometer")
+        VStack {
+            HStack {
+                Spacer()
+                if logStarting {
+                    Text("Under Logging")
+                }
+                Spacer()
+                
+                VStack {
+                    Toggle(isOn: $logStarting) {
+                        Text("")
+                    }
+                    Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("Timing")) {
+                        Text("Immediately").tag(1)
+                        Text("After 5 sec").tag(2)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("Auto")) {
+                        Text("Self").tag(1)
+                        Text("Session").tag(2)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                }
+                Spacer()
+                
+            }.padding(.leading)
+            
+            VStack(alignment: .leading) {
+                Text("Accelerometer")
+                Text("Gyroscope")
+                Text("Magnetometer")
+            }.padding(.leading)
         }
     }
 }
